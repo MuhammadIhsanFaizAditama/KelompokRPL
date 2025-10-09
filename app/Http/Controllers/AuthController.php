c2f4e7f984d7c5a5b63da1bae732e53011fac589
Faiz <hshs85812@gmail.com>
2025-10-09 13:06:49 +0800

    .

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function showRegister()
+    {
+        return view('auth.register');
+    }
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:user',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        \App\Models\User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'role' => 'student', // Set default role to student
+        ]);
+
+        return redirect()->route('login')->with('success', 'Registration successful. Please login.');
+    }
 }
